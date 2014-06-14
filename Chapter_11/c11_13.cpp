5c0fe67976c5ba95376a966ab5365c75dc44f6cd
onthewing <onthewinghgd@gmail.com>
2014-06-15 00:27:06 +0800

    selection algorithm

---
+++ Chapter_11/c11_13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void partition(vector<int> &V, int low, int high, int k) {
+  int m = low;
+  int pivot = V[0];
+  for (int i = low; i < high; i++) {
+    if (V[i] > pivot) {
+      swap(V[i], V[++m]);
+    }
+  }
+  swap(V[m], V[0]);
+  if (m == k - 1)
+    return;
+  else if (m > k - 1)
+    return partition(V, low, m-1, k);
+  else
+    return partition(V, m + 1, high, k - m - 1);  
+} 
