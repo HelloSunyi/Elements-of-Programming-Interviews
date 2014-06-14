e456daff23b71ee8a16a7dd46caf7d56ae0d9950
onthewing <onthewinghgd@gmail.com>
2014-06-15 01:28:35 +0800

    find value appers once while others apper twice

---
+++ Chapter_11/c11_16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int find_onece(const vector<int>& V) {
+  int once = 0;
+  for (int i - 0; i < V.size(); i++) {
+    onece ^= V[i];
+  }
+  return once;
+}
