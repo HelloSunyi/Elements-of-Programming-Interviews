1b5e4a138ad1118f37db37294c4aa51d02fe784d
onthewing <onthewinghgd@gmail.com>
2014-06-14 19:20:57 +0800

    Chapter 11

--- Chapter_11/c11_2.cpp
+++ Chapter_11/c11_2.cpp
@@ -9,9 +9,23 @@ int find_first_larger(const vector<int>& v, int k) {
   while (low < high) {
     int mid = low + (high - low)/2;
     if (v[mid] <= k)
-       mid = low + 1;
+       low = mid + 1;
     else
        high = mid;
   }
   return (v[low] > k) ? low : -1;
+}
+
+int solution2 (const vector<int>& v, int k) {
+  int low = 0, high = v.size() - 1;
+  int ret = -1;
+  while (low <= high) {
+    if (v[mid] <= k)
+       low = mid + 1;
+    else {
+      ret = mid;
+      high = mid - 1;
+    }
+  }
+  return ret;
 }
