bb8326595cd05ff025639d3602fb4047107779b1
onthewing <onthewinghgd@gmail.com>
2014-06-15 22:17:33 +0800

    find element appears once while others appear twice

--- Chapter_11/c11_17.cpp
+++ Chapter_11/c11_17.cpp
@@ -3,4 +3,14 @@
 
 using namespace std;
 
-
+ing find_one(vector<int> V) {
+  int one = 0, two = 0, three = 0;
+  for (int i = 0; i < V.size(); i++) {
+    two = two | one & A[i];
+    one = one ^ A[i];
+    three = tow & one;
+    one = one & ~three;
+    two = two & ~three;
+  }
+  return one;
+}
