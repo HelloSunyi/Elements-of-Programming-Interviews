54c836e2ab9a5d483fa46815f6920a75d25cdc9b
onthewing <onthewinghgd@gmail.com>
2014-06-17 23:58:15 +0800

    Chapter 13

--- Chapter_13/c13_2.cpp
+++ Chapter_13/c13_2.cpp
@@ -1,37 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
-struct point {
-  int value;
-  bool isEnd;
-};
-
+// The key point is use pointer rather than string variable to swap.
 struct cmp {
-  bool operator() (const point& a, const point& b) {
-    return (a.value == b.value) ? b.isEnd : (a.value < b.value);
+  bool operator() (string* s1, string* s2) {
+    return (*s1) < (*s2);
   }
 };
 
-int find_max_intersection(vector<pair<int, int> > V) {
-  vector<point> tmp;
-  for (int i = 0; i < V.size(); i++) {
-    struct t1, t2;
-    t1.value = V[i].first;
-    t1.isEnd = false;
-    t2.value = V[i].second;
-    t2.isEnd = true;
-    tmp.push_back(t1);
-    tmp.push_back(t2);
+void sort(vector<string> S) {
+  vector<string*> sptr;
+  for (int i = 0; i < S.size(); i++) {
+    sptr.push_back(&S[i]);
   }
-  sort(tmp.begin(), tmp.end(), cmp);
-  int ret = INT_MIN;
-  int count = 0;
-  for (int i = 0; i < tmp.size(); i++) {
-    count += (tmp[i].isEnd) ? -1 : 1;
-    ret = max(ret, count);
-  }
-  return ret;
+  sort(sptr.begin(), sptr.end(), cmp);
 }
