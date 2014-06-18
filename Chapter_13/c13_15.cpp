bc8fc00c856193a48960b90c8b1c5c8283523b02
onthewing <onthewinghgd@gmail.com>
2014-06-18 21:15:06 +0800

    flipping sort

---
+++ Chapter_13/c13_15.cpp
@@ -0,0 +1,4 @@
+// The solution is similar with insertion sort:
+// For the biggest one, move it to the top position(one flip) and then move to the bottom(one flip, totally two flips).
+// Then the second biggest one, and etc...
+// The time complexity is O(n2)
