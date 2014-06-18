cd05f30b414f7c1a038686f346f57ae9b2cc6a6a
gdhan <gdhan@clustertech.com>
2014-06-18 14:57:24 +0800

    Chapter 13

---
+++ Chapter_13/c13_1.cpp
@@ -0,0 +1,6 @@
+// - A large array whose entries are random numbers: quick sort.
+// - A samll array: insert sort.
+// - A large array already sorted: heap sort.
+// - A large colletcion of integers drawn from a small range: couting sort.
+// - A large collection of numbers most of which are duplicated: BST.
+// - Stability is required: merge sort.
