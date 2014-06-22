ea35454209a9a795f1150569f8496d0997dd98cf
onthewing <onthewinghgd@gmail.com>
2014-06-23 00:53:08 +0800

    merge two BST

---
+++ Chapter_14/c14_9.cpp
@@ -0,0 +1,5 @@
+// 1. The easist way to merge is sorted linked list.
+// 2. So, we need to transfer BST to linked list firstly,
+// it is eazy to transfer from BST to sorted doubly linked list(c14_7).
+// 3. Then merge two doubled linked list.
+// 4. Generate BST from sorted doubly linked list(c14_8).
