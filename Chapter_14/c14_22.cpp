93dd6a3f0a0b9c0fe591697b03e650694ca70dc4
gdhan <gdhan@clustertech.com>
2014-06-25 18:14:01 +0800

    query server logs

--- Chapter_14/c14_22.cpp
+++ Chapter_14/c14_22.cpp
@@ -4,5 +4,6 @@
 using namespace std;
 
 // As solution c14_14, use two BST(with parent point) building on log interval's left end
-// and right end, for search intervals in [L, U], first search left and right ends in two BST,
+// and right end, for search intervals in [L, U], first search left ends that is less than U
+// and right ends that is more than L in two BST,
 // and use a hash to find the ends in the same interval!
