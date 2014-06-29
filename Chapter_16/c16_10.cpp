88d844b6d95ed2383013fd767e00aadd87f596eb
onthewing <onthewinghgd@gmail.com>
2014-06-29 20:40:48 +0800

    shortest path

---
+++ Chapter_16/c16_10.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+$include <vector>
+
+using namespace std;
+
+// Use dijkstra's algorithm to sovle this problem: flight plays the role of edge and city plays the role of vertice.
