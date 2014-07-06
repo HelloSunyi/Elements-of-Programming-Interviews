08f880c233aac46f4662a66c8e209ff4474aad5e
onthewing <onthewinghgd@gmail.com>
2014-07-06 18:12:16 +0800

    ununiformed probability

---
+++ Chapter_20/c20_5.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <vector>
+
+// 1. Compute the prefix sum of probability.
+// 2. Calculate the probability by [0,1] random genrator, find which interal contains the random probability.
