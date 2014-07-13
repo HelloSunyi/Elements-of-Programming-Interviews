0f40a708dfcb20ff046f0e44a261b5b4c5675e45
onthewing <onthewinghgd@gmail.com>
2014-07-13 22:54:32 +0800

    external sort

---
+++ Chapter_18/c18_14.cpp
@@ -0,0 +1,2 @@
+// For data could be held in one machine, we use external sort: divide the data into several file, sort each file and merge all files with heap.
+// For data could not be held in one machine, we use one machine to handle a range of data.
