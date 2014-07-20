f366d3698b880f69f9d8b0322b93834193327ad3
onthewing <onthewinghgd@gmail.com>
2014-07-20 23:08:30 +0800

    Chapter 19

---
+++ c19_1.cpp
@@ -0,0 +1,4 @@
+// Divde the target image into s*s sized squres, and compute the average color the squre
+// Compute the average color of each tile/image.
+// For each squre, find the closest matched image, O(nm) time complexity.
+// Optimization: compute in parallel, use search tree.
