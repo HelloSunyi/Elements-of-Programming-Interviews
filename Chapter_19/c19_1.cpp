ec61f8d681956ebd320781c58b0564ce5723836b
onthewing <onthewinghgd@gmail.com>
2014-07-23 22:20:56 +0800

    chapter 19

---
+++ Chapter_19/c19_1.cpp
@@ -0,0 +1,4 @@
+// Divide the figure into n*n squares, for each square compute the average color.
+// For each tile/image find the one has smallest distance with the square.
+// Could use parallelization to speedup.
+
