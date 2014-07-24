5d3d2c2714dcaf2acb8b41d0518a5d8da56407e7
onthewing <onthewinghgd@gmail.com>
2014-07-25 00:10:02 +0800

    read file

---
+++ Chapter_19/c19_7.cpp
@@ -0,0 +1,2 @@
+// For reading the last n lines, use a n lines sized queue, each time read a time and push into the end of queue;
+// When the queue is full, for a new comming line, delete the last line in the end of queue, untill finish reading the file
