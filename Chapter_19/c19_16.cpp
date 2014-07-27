6612473eb3ecaf85b650902f60a737e4a71be1e1
onthewing <onthewinghgd@gmail.com>
2014-07-27 18:15:02 +0800

    poker system

---
+++ Chapter_19/c19_16.cpp
@@ -0,0 +1,4 @@
+// Front-end: UI design, user account management, authorization, log-in, add friends, talk. UX
+// Backend: HTTP server, data base, card shuffle algorithm: equal random numer generator, data consistency
+// One big chanllenge: Fault-tolerance: make some check-points for roll-back and data backup
+// How to deal with cheat
