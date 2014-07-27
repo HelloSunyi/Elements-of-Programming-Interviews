507a52b47a3d3f878fae0539809177e9e71a330d
onthewing <onthewinghgd@gmail.com>
2014-07-27 17:31:23 +0800

    design online ads system

---
+++ Chapter_19/c19_12.cpp
@@ -0,0 +1,3 @@
+// Design goals: provide user most related ads, user account authorizaion, performance report, minize cost, system interaction, ui design
+// Front-end: php, js design
+// Backend: OS, http server(Apache), database(MySQL)
