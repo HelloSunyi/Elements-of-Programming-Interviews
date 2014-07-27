cdb67bb90bc0280e99915fcc6085a783d63f46a7
onthewing <onthewinghgd@gmail.com>
2014-07-27 17:44:54 +0800

    recommendation system

---
+++ Chapter_19/c19_13.cpp
@@ -0,0 +1,4 @@
+// List the most popular and latest articles.
+// Classify the articles: accroding to the keywords/tags into subset; users could add tags. Provide similar articles in the subset.
+// System find the similar articles by matching same words: ignore words like: is are and. Give high weight to some significant words
+// Analyze server log, if many users read article A and B in a time interval, so when user read acticle A, recommend B
