175bc00fe6807e135b708aeffbc379155efceaf3
onthewing <onthewinghgd@gmail.com>
2014-07-27 16:02:53 +0800

    vedio invalidence

---
+++ Chapter_19/c19_8.cpp
@@ -0,0 +1,3 @@
+// 1. Macth the title: string comparison
+// 2. Compare the vedio: convert to a the same format, then use a vedio matching algorithm
+// 3. Encourage user to report invaild vedio and rewards some money
