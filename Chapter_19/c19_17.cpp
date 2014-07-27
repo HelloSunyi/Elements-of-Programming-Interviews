12eb67f4f814a694ad2ae20c2712a183b3c8027d
onthewing <onthewinghgd@gmail.com>
2014-07-27 18:19:28 +0800

    WWW

---
+++ Chapter_19/c19_17.cpp
@@ -0,0 +1,4 @@
+// For url, first convert to the ip address though DNS server
+// send request to the server through HTTP, TCP/IP protocal
+// Parse the returned data, and show the data
+// Security, cookies, HTML style, data format, script execution
