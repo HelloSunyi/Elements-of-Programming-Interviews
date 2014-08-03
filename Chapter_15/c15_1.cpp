9e27762f1f293bc8a5e292cd6cd0003ef274a749
onthewing <onthewinghgd@gmail.com>
2014-08-03 18:42:01 +0800

    skyline problem

---
+++ Chapter_15/c15_1.cpp
@@ -0,0 +1,3 @@
+// Use divide and conque to solve this problem
+// Divide skylines into two parts, handle the two parts seperately
+// Merge the two parts, merge intersection intervals. all intervals are sorted
