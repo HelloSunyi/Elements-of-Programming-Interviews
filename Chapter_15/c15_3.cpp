1cc2d4d518bb7e6add5e1af8ee037fc08e6231fe
onthewing <onthewinghgd@gmail.com>
2014-08-03 18:11:02 +0800

    closest pairs

---
+++ Chapter_15/c15_3.cpp
@@ -0,0 +1,4 @@
+// Classical divide and conque problem
+// Sort according to x axis, didive into two parts by x = p
+// For each part, find the closest distance d1, d2, d = min(d1, d2);
+// Collect points which x [p-d, p+d], for these points, find closest distance by sorting y axis.
